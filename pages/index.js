e9a211cac941089b4a4b6c36144afc019ac51002
Sam Poder <sam.r.poder@gmail.com>
2022-11-04 09:51:20 +0800

    Space in Cyber City

--- pages/index.js
+++ pages/index.js
@@ -150,7 +150,7 @@ export default function Main() {
                 >
                   Dec. 30 2022 to Jan. 1 2023
                 </Box>
-                <Box sx={{ fontWeight: 800 }}>CyberCity, Delhi NCR </Box>
+                <Box sx={{ fontWeight: 800 }}>Cyber City, Delhi NCR </Box>
                 <Box>₹12.5 Lakh in travel stipends </Box>
                 <Box> Doors open at 6PM; 42-hours long </Box>
               </Flex>
