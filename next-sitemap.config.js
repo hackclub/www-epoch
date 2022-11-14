c3f035b018713652f524d664407783cadab4724a
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-11-14 13:04:04 +0800

    Add Sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://lioncityhacks.com',
+  generateRobotsTxt: true, // (optional)
+  // ...other optionsfff
+}
